65133f3b037ba8b881a31ac57d6fbf4405005cf0
JabSysEmb <abduljabbar.sha97@gmail.com>
2022-01-30 18:17:12 +0300

    Event Handling in React Lab submission

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
 // Code EyesOnMe Component Here
+const EyesOnMe = () => {
+    const onBlurHanlder = () => console.log('Hey! Eyes on me!')
+    const onFocusHandler = () => console.log('Good!')
+    return(
+        <button onBlur={onBlurHanlder} onFocus={onFocusHandler}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+
+const Keypad = ({}) => {
+    const passwordInputChangeHanlder = () => console.log('Entering password...')
+    return(
+        <input type="password" onChange={passwordInputChangeHanlder}/>
+    )
+}
+
+export default Keypad;
